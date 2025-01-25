7ab14ace2b959e1f5272297dd8d2d5363eb42483
Rapeepat Treerattrakul <rapeepat.ptpk@gmail.com>
2025-01-25 17:57:04 +0700

    Fetch word data from API

---
+++ script.js
@@ -0,0 +1,27 @@
+const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
+const result = document.getElementById('result')
+const sound = document.getElementById('sound')
+const btn = document.getElementById('search-btn')
+
+btn.addEventListener('click', () => {
+    const inpWord = document.getElementById('inp-word').value
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            result.innerHTML = `
+                <div class="word">
+                    <h3>${inpWord}</h3>
+                    <button onClick='playSound()'>
+                        <i class="fa-solid fa-volume-high"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>${data[0].phonetics[0].text}</p>
+                </div>
+                <p class="word-meaning">${data[0].meanings[0].definitions[0].definition}</p>
+                <p class="word-example">${data[0].meanings[0].definitions[0].example || ''}</p>
+            `
+        })
+})
