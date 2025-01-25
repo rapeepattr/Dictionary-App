d918205a1f7fc54dba9dcbf0724b3f0abab6250c
Rapeepat Treerattrakul <rapeepat.ptpk@gmail.com>
2025-01-25 18:05:02 +0700

    Add play sound function

--- script.js
+++ script.js
@@ -11,17 +11,24 @@ btn.addEventListener('click', () => {
             console.log(data)
             result.innerHTML = `
                 <div class="word">
-                    <h3>${inpWord}</h3>
+                    <h3>${inpWord || ''}</h3>
                     <button onClick='playSound()'>
                         <i class="fa-solid fa-volume-high"></i>
                     </button>
                 </div>
                 <div class="details">
-                    <p>${data[0].meanings[0].partOfSpeech}</p>
-                    <p>${data[0].phonetics[0].text}</p>
+                    <p>${data[0].meanings[0].partOfSpeech || ''}</p>
+                    <p>${data[0].phonetics[0].text || ''}</p>
                 </div>
-                <p class="word-meaning">${data[0].meanings[0].definitions[0].definition}</p>
+                <p class="word-meaning">${data[0].meanings[0].definitions[0].definition || ''}</p>
                 <p class="word-example">${data[0].meanings[0].definitions[0].example || ''}</p>
             `
+            sound.setAttribute('src', data[0].phonetics[0].audio || '')
+        }).catch(() => {
+            result.innerHTML(`<h3 class='error'>Couldn't Find The Word</h3>`)
         })
 })
+
+function playSound() {
+    sound.play
+}
